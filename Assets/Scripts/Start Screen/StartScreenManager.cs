c5061037a89a12e41aeacc0e217582e825101344
AjBetito <ecbetito@gbox.adnu.edu.ph>
2024-12-24 03:21:03 +0800

    Added Game Over Screen
    It should work if the Title Screen scene is in the build settings.

--- Assets/Scripts/Start Screen/StartScreenManager.cs
+++ Assets/Scripts/Start Screen/StartScreenManager.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class StartScreenManager : MonoBehaviour
 {
-    // Function to load the first game scene
     public void StartGame()
     {
-        // Replace "GameScene" with the name of your first game scene
         SceneManager.LoadScene("GameScene");
     }
 
-    // Function to quit the application
     public void QuitGame()
     {
         #if UNITY_EDITOR
-        // If running in the Unity Editor, stop playing the game
         UnityEditor.EditorApplication.isPlaying = false;
         #else
-        // Quit the application
         Application.Quit();
         #endif
     }
+
+    // Single LoadTitleScreen method
+    public void LoadTitleScreen()
+    {
+        SceneManager.LoadScene("Title Screen");
+    }
 }
+
